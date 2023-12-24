038ce96004da0b7c9c26afaa20e652a4fe14a132
valentincpopa <38735469+valentincpopa@users.noreply.github.com>
2023-12-24 16:36:12 +0200

    day-4

---
+++ day-4/day-4-p1.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+
+int getAdvanceOffset(char *numberSets);
+
+int main()
+{
+    FILE *file;
+    file = fopen("./input.txt", "r");
+    int bufferLength = 255;
+    char buffer[bufferLength];
+    char *numberSets;
+
+    if (file == NULL)
+    {
+        return -1;
+    }
+
+    double total = 0;
+
+    while (fgets(buffer, bufferLength, file))
+    {
+        int advanceOffset = getAdvanceOffset(buffer);
+        long gameId = strtol((buffer + advanceOffset), &numberSets, 10);
+
+        int *winningNumbers = calloc(100, sizeof(int));
+        int *numbersInHand = calloc(100, sizeof(int));
+
+        char *numberSet = strtok((numberSets), "|");
+        int winningNumberCount = 0;
+        int setId = 0;
+
+        while (numberSet != NULL)
+        {
+            setId++;
+            int advanceOffset = getAdvanceOffset(numberSet);
+
+            while (advanceOffset > -1)
+            {
+                if (setId == 1)
+                {
+                    long winningNumber = strtol((numberSet + advanceOffset), &numberSet, 10);
+                    winningNumbers[winningNumberCount] = winningNumber;
+                    winningNumberCount++;
+                }
+                else
+                {
+                    long numberInHand = strtol((numberSet + advanceOffset), &numberSet, 10);
+                    numbersInHand[numberInHand]++;
+                }
+                advanceOffset = getAdvanceOffset(numberSet);
+            }
+
+            numberSet = strtok(NULL, "|");
+        }
+
+        double twosPower = -1;
+
+        for (int i = 0; i < winningNumberCount; i++)
+        {
+            if (numbersInHand[winningNumbers[i]] == 1)
+            {
+                twosPower++;
+            }
+        }
+
+        if (twosPower > -1)
+        {
+            total += pow(2, twosPower);
+        }
+    }
+
+    printf("%1.f", total);
+}
+
+int getAdvanceOffset(char *numberSets)
+{
+    int advanceOffset = 0;
+    while (1)
+    {
+        advanceOffset++;
+
+        if (*(numberSets + advanceOffset) == '\000')
+        {
+            return -1;
+        }
+
+        if (isdigit(*(numberSets + advanceOffset)))
+        {
+            return advanceOffset;
+        }
+    }
+}
